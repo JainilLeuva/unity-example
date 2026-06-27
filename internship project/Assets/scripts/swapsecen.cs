2972c2c467c10680dec8dbc353dc9c4fa6cb35c8
ASHISH-11A <ashishslalwani672@gmail.com>
2026-06-27 13:59:09 +0530

    sound stopping problem solved

---
+++ internship project/Assets/scripts/swapsecen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SwapScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (SceneManager.GetActiveScene().name == "stage 1")
+            BGmusic.instance.GetComponent<AudioSource>().Play();
+        if (SceneManager.GetActiveScene().name == "stage 1")
+            BGmusic.instance.GetComponent<AudioSource>().Pause();
+        //BGmusic.instance.GetComponent<AudioSource>().Play();
+        if (SceneManager.GetActiveScene().name == "stage 2")
+            BGmusic.instance.GetComponent<AudioSource>().Pause();
+
+    }
+}
